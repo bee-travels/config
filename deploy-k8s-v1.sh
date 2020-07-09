f2b1bf8cf58a13f3f45dd5a24426f648f023a5e0
Grace Kim <gracekim@ibm.com@Graces-MacBook-Pro.local>
2020-07-08 17:38:30 -0700

    v1/v2 deploy script, gateway/dr/vs

--- deploy-k8s-v1.sh
+++ deploy-k8s-v1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pwd
 kubectl apply -f k8s/currencyexchange-deploy.yaml
-kubectl apply -f k8s/destination-v2-deploy.yaml
-kubectl apply -f k8s/hotel-v2-deploy.yaml
-kubectl apply -f k8s/carrental-v2-deploy.yaml
+kubectl apply -f k8s/destination-v1-deploy.yaml
+kubectl apply -f k8s/hotel-v1-deploy.yaml
+kubectl apply -f k8s/carrental-v1-deploy.yaml
 kubectl apply -f k8s/ui-deploy.yaml
 kubectl apply -f k8s/destination-service.yaml
 kubectl apply -f k8s/hotel-service.yaml
