23b148eeb9e64700cb76522402839245ba058705
Mofizur Rahman <moficodes@gmail.com>
2020-08-03 20:13:28 -0400

    updated golden iamge version

--- deploy-k8s-v1.sh
+++ deploy-k8s-v1.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
-pwd
 kubectl apply -f k8s/currencyexchange-deploy.yaml
-kubectl apply -f k8s/destination-v2-deploy.yaml
-kubectl apply -f k8s/hotel-v2-deploy.yaml
-kubectl apply -f k8s/carrental-v2-deploy.yaml
+kubectl apply -f k8s/destination-v1-deploy.yaml
+kubectl apply -f k8s/hotel-v1-deploy.yaml
+kubectl apply -f k8s/carrental-v1-deploy.yaml
 kubectl apply -f k8s/ui-deploy.yaml
 kubectl apply -f k8s/destination-service.yaml
 kubectl apply -f k8s/hotel-service.yaml
 kubectl apply -f k8s/carrental-service.yaml
 kubectl apply -f k8s/currencyexchange-service.yaml
 kubectl apply -f k8s/ui-service.yaml
-kubectl apply -f k8s/ui-ingress.yaml
